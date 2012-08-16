f6f87dfc930488fc0b44ac50a1c7d5260e93a7ab
geshtop <g@geshtop.com>
2012-08-16 19:55:43 +0300

    change again the domain for smtp config

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'supersefer.herokuapp.com'
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
