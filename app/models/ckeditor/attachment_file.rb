19be43e71d0ff7c9eb87d8369e2c709bee582aec
geshtop <g@geshtop.com>
2012-09-03 21:30:41 +0300

    add the option to upload files using ckeditor

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,8 +1,13 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
                     :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
-  
+                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename",
+			      :storage => Rails.env.production? ? :s3 : :filesystem,
+			    :s3_credentials => {
+			      :bucket            => ENV['S3_BUCKET_NAME'],
+			      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+			      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+			    }
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
 	
@@ -10,3 +15,6 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
 	  @url_thumb ||= Ckeditor::Utils.filethumb(filename)
 	end
 end
+
+
+
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,12 +2,21 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-	                  :styles => { :content => '800>', :thumb => '118x100#' }
+	                :styles =>{
+					      :thumb => "80x120>", :medium => "160x240>", :large => "352X528>"
+					    },
+					    :storage => Rails.env.production? ? :s3 : :filesystem,
+					    :s3_credentials => {
+					      :bucket            => ENV['S3_BUCKET_NAME'],
+					      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+					      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+					    }
 	
-	validates_attachment_size :data, :less_than => 2.megabytes
+	validates_attachment_size :data, :less_than => 3.megabytes
 	validates_attachment_presence :data
 	
 	def url_content
-	  url(:content)
+	  url(:original)
 	end
 end
+
